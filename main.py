947073eab0df3887fb804a67addcf177ba5f8f95
CaioXyZ <caioreis29974@gmail.com>
2025-03-22 18:41:03 -0300

    first code

--- main.py
+++ main.py
@@ -1,32 +1,11 @@
-# 📥 TubeSnatch - YouTube Video Downloader
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
 
-# Place to install dependencies
-# (Aqui serão instaladas as bibliotecas necessárias)
+url = ''
+save = 'saves'
 
-# Import necessary modules
-# (Aqui farei as importações de módulos e bibliotecas)
+yt = YouTube(url, on_progress_callback= on_progress)
+print(yt.title)
 
-# GUI Setup
-# (Configuração da interface gráfica do aplicativo)
-
-# Function to select video quality
-# (Função para permitir ao usuário escolher a qualidade do vídeo)
-
-# Function to download individual videos
-# (Função para baixar vídeos individuais)
-
-# Function to handle playlist downloads (To be implemented later)
-# (Função para lidar com downloads de playlists - Será implementado depois)
-
-# Function to manage file naming (Original title will be used)
-# (O nome do arquivo seguirá o título original do vídeo)
-
-
-# Function to log downloads (Save details like video title, URL, date, and status)
-# (Função para registrar os downloads - Salva detalhes como título, URL, data e status)
-
-# Main execution (Runs the app)
-# (Execução principal do aplicativo)
-
-# GUI loop to keep the app running
-# (Loop da interface gráfica para manter o app aberto)
+ys = yt.streams.get_highest_resolution()
+ys.download(output_path=save)
